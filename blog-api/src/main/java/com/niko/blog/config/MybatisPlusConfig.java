c1099de542b8aa4e8924acec14dc99a6c0d79eb0
Bo Geng <b.geng@student.utwente.nl>
2021-11-25 23:33:08 +0800

    finished initial structure for Blog

---
+++ blog-api/src/main/java/com/niko/blog/config/MybatisPlusConfig.java
@@ -0,0 +1,18 @@
+package com.niko.blog.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.niko.blog.mapper")
+public class MybatisPlusConfig {
+
+    //plugin for sidebar
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+}
